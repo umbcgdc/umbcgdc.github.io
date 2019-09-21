16712d0de4c7227b0b3c5d3e25910b8b70797ad9
kc <kaelachen22@gmail.com>
2019-09-21 17:00:22 -0400

    @import google fonts instead of file include

--- readme/readme.js
+++ readme/readme.js
@@ -10,8 +10,6 @@ var tutorial = new Vue({
       ['  <meta charset="utf-8">', 'Choose character encoding'],
       ['  <meta name="viewport" content="width=device-width, initial-scale=1">', 'Scale for mobile devices'],
       ['  <link rel="shortcut icon" href="ui/logo.ico" type="image/x-icon">', 'link to the icon that displays on the tab'],
-      ['  <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700" rel="stylesheet">', 'link to the headings font'],
-      ['  <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:300" rel="stylesheet"> ', 'link to the main font'],
       ['  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">', 'link to the material design icons (you can leave this out if there are no icons on your page'],
       ['  <link href="css/main.css" rel="stylesheet">', 'link to the main css styles'],
       ['  <title>UMBC Game Developers Club</title>', 'title to display on the tab'],
